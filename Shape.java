dda065e6bea0f961647f2f5aa98831b52098cb0c
Omer Zeliger <omer@Omers-MacBook-Pro.local>
2019-02-25 17:50:31 -0500

    fsds

--- Shape.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface Shape {
-  List<Motion> getMotions();
-  void addMotion(int time, int x, int y, int width, int height,
-                 int red, int green, int blue);
-  void deleteLastMotion();
-  boolean sameShape(String name);
-}
